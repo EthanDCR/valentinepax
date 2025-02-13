0654101ddb8f23a6f37e64ee8fb9c0ead9d23789
EthanDCR <clinteth.dev@gmail.com>
2025-02-12 20:56:13 -0600

    finished

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "No way!",
   titleModar: "Thank you!",
   descriptionModar:
-    "<Thank you for being mine.<br>I love you more than coffee.<br>I met you. I liked you. I love you. I’m keeping you.<br>You may hold my hand for a while, but you hold my heart forever.<br>Whenever I'm with you, wherever we are, I'm home.</br><br></br> Love you Jade <3",
+    "<br>I met you. I liked you. I love you. I’m keeping you.<br>You may hold my hand for a while, but you hold my heart forever.<br>Whenever I'm with you, wherever we are, I'm home.</br><br></br> Love you Morgan <3",
 };
