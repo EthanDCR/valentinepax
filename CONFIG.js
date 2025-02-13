fd6de9fbe1660353daec1fc763b21e1406597e4c
EthanDCR <clinteth.dev@gmail.com>
2025-02-12 20:50:35 -0600

    initial commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Hey, I have something to ask you.Will you be my Valentine?",
+  descriptionHeader:
+    "If you stay silent and leave, that means you’ve already said YES!",
+  buttonYes: "Yes, of course!",
+  buttonNo: "No way!",
+  titleModar: "Thank you!",
+  descriptionModar:
+    "<Thank you for being mine.<br>I love you more than coffee.<br>I met you. I liked you. I love you. I’m keeping you.<br>You may hold my hand for a while, but you hold my heart forever.<br>Whenever I'm with you, wherever we are, I'm home.</br><br></br> Love you Jade <3",
+};
